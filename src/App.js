4b116e66bffd8abb7acb5b1a3d3d34a00ff11e2f
MuttuKarani <muttukarani86@gmail.com>
2024-10-22 15:49:30 +0530

    Some changes in Navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 // src/App.js
 import "./App.css";
 //import "./components/modules/Navigation";
-import Navbar  from './components/modules/Navigation';
-import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from "./components/modules/Navigation";
+import "bootstrap/dist/css/bootstrap.css";
+import Home from "./components/views/Home";
+import Topheader from "./components/modules/Topheader";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navbar />
-      </header>
-      
+      <Topheader />
+      <Navbar />
+      <Home />
     </div>
-
-
   );
 }
-/*        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello</h1> */
 export default App;
