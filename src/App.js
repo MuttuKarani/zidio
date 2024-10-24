5a6d74b4d500abc2c08bfd3fe2b1667a3e40458c
MuttuKarani <muttukarani86@gmail.com>
2024-10-24 10:58:54 +0530

    Navigation bar

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@
 import "./App.css";
 import Navbar from "./components/modules/Navigation";
 import "bootstrap/dist/css/bootstrap.css";
-import Home from "./components/views/Home";
+import Home from "./components/views/HomePage";
 
-import Services from "./components/views/Services";
-import Contact from "./components/views/ContactUs";
+import Services from "./components/views/ServicesPage";
+import Contact from "./components/views/ContactPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AboutUs from "./components/views/AboutUs";
+import AboutUs from "./components/views/AboutPage";
 import Topheader from "./components/modules/Topheader";
 
 function App() {
   return (
     <div className="App">
-      <Topheader />
-      <Navbar />
       <Router>
+        <Topheader />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
--- src/components/modules/Navigation.js
+++ src/components/modules/Navigation.js
@@ -1,15 +1,12 @@
-
+// Navigation.js
 import React from "react";
-import {Navbar, Nav, Container, Button} from "react-bootstrap";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/Navigation.css";
-//import Link from "react-router-dom"
-
-
 
 const Navigation = () => {
   return (
-
     <Navbar bg="light" expand="lg" className="shadow-sm">
       <Container>
         {/* Logo */}
@@ -22,38 +19,35 @@ const Navigation = () => {
 
         {/* Navigation links */}
         <Navbar.Collapse id="navbar-nav">
-        
-
-         <Nav className="me-auto">
-            
-            <Nav.Link href="#Home" className="nav-link">Home</Nav.Link>
-            <Nav.Link href="#About" className="nav-link">About</Nav.Link> 
-            <Nav.Link href="#services" className="nav-link">Services</Nav.Link>
-            <Nav.Link href="#contact" className="nav-link">Contact</Nav.Link>
+          <Nav className="me-auto">
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+            <NavLink to="/about" className="nav-link">
+              About
+            </NavLink>
+            <NavLink to="/services" className="nav-link">
+              Services
+            </NavLink>
+            <NavLink to="/contact" className="nav-link">
+              Contact
+            </NavLink>
           </Nav>
 
-
           {/* Search icon */}
-
-            <Nav className="me-3">
+          <Nav className="me-3">
             <Nav.Link href="#search" className="nav-link">
               <i className="bi bi-search"></i>
             </Nav.Link>
-          </Nav> 
+          </Nav>
 
- 
           {/* Button */}
           <Button variant="primary" href="#quote">
             Get A Quote
           </Button>
-         
         </Navbar.Collapse>
-      
       </Container>
     </Navbar>
-
-
-
   );
 };
 
